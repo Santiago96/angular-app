f35f684b682897b8063e3ba818735921196ffbbc
Santiago Collazos Barrera <scollazos@belatrix.com>
2019-09-27 17:02:38 -0500

    Charged project angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a angular';
+
+  course = 'Curso Spring 5 con Angular 7';
+  student = 'Santiago Collazos';
+}
