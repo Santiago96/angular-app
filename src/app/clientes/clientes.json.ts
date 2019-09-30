d07619b70fe8be22c3ff3f5ce13c3f73421a2fdb
Santiago Collazos Barrera <scollazos@belatrix.com>
2019-09-30 10:14:40 -0500

    Adding files like cliente.service to handle information with decorator Observable besides all the demo data was to moved to json file to be getting through const CLIENTES

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,7 @@
+
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+   {id: 1, nombre: 'Santiago', apellido:'Collazos', email: 'santicollazosbarrera@hotmail.com', createAt: '2019/05/06'},
+   {id: 2, nombre: 'Julian', apellido:'Collazos', email: 'jdcb94@hotmail.com', createAt: '2019/05/17'},   
+  ];
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+  styleUrls: ['./directiva.component.css']
+})
+export class DirectivaComponent{
+
+  listaCurso: string[] = ['TypeScript','.Net','Java','PHP','C++','Python','C#','Javascript']	;
+  habilitar: boolean = true;
+  constructor() { }  
+
+
+  setHabilitar(){
+  	this.habilitar = !this.habilitar;
+  }
+
+}
