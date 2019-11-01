dd5fcd823ceb05a7de74a0548de8dfe6b2697fea
Santiago Collazos Barrera <santicollazosbarrera@hotmail.com>
2019-10-31 23:52:18 -0500

    Finished consume backend with service

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form',
@@ -11,14 +14,16 @@ export class FormComponent implements OnInit {
   private title: string = "Crear Cliente"
 
 
-  constructor() { }
+  constructor(private clienteService: ClienteService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
   public create(): void{
-  	console.log("clicked");
-  	console.log(this.cliente);
+  	this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+      )
   }
 
 }
